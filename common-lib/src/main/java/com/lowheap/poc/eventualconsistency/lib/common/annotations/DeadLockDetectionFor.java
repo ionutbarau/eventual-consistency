83b30de75a15999fc207a4aa3f390444cae096a0
Ionut Barau <ionutbarau@gmail.com>
2023-12-16 13:18:23 +0200

    Update DeadLockDetectionFor.java

--- common-lib/src/main/java/com/lowheap/poc/eventualconsistency/lib/common/annotations/DeadLockDetectionFor.java
+++ common-lib/src/main/java/com/lowheap/poc/eventualconsistency/lib/common/annotations/DeadLockDetectionFor.java
@@ -1,4 +1,8 @@
 package com.lowheap.poc.eventualconsistency.lib.common.annotations;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a method as the deadlock detection method for a list of sagas.
