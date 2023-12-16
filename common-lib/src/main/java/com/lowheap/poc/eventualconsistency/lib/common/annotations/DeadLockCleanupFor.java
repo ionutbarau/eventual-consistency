25f277948c2994c9b83a181bea61b1bc2353042e
Ionut Barau <ionutbarau@gmail.com>
2023-12-16 13:18:12 +0200

    Update DeadLockCleanupFor.java

--- common-lib/src/main/java/com/lowheap/poc/eventualconsistency/lib/common/annotations/DeadLockCleanupFor.java
+++ common-lib/src/main/java/com/lowheap/poc/eventualconsistency/lib/common/annotations/DeadLockCleanupFor.java
@@ -1,4 +1,8 @@
 package com.lowheap.poc.eventualconsistency.lib.common.annotations;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a method as a dead-lock cleanup step for a list of sagas.
@@ -7,6 +11,8 @@ package com.lowheap.poc.eventualconsistency.lib.common.annotations;
  * Date: 07.12.2023.
  * Time: 11:41
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface DeadLockCleanupFor {
 
     String[] sagas() default "";
